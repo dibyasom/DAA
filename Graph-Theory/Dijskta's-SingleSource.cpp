920be8c1431adc9a3b883ec0ebaf8709ff35edf5
vhaegar2615 <53910913+vhaegar1526@users.noreply.github.com>
2020-12-03 19:46:52 +0530

    Removed binaries, and added collapse-find to disjoint sets.

--- Graph-Theory/Dijskta's-SingleSource.cpp
+++ Graph-Theory/Dijskta's-SingleSource.cpp
@@ -2,17 +2,69 @@
 
 using namespace std;
 
-int main(void){
+#define infinity INT_MAX;
 
-	int e; cout << "NUmber of edges> "; cin >> e;
-	int 
-	for(int i=1; i<=e; i++)
-		cout << " " << i << " ";
-	for(int i=1; i<=e; i++){
-		cout << i << " ";
-		cin >> 
+class node{
+	public:
+		int weight, src, dest, status;
+		node(int w, int s, int d){
+			weight= w;
+			src= s;
+			dest= d;
+		}
+};
+
+int find_min(int *adj_nodes, int* visited, int no_of_vertices){
+	int min= infinity, min_ind=-1;
+	for(int i=0; i<no_of_vertices; i++)
+		if(adj_nodes[i]<=min && !visited[i]){
+		   	min=adj_nodes[i];
+			min_ind=i;
+		}
+	return min;
+}
+
+void dijsktra_single_source(node** edges, int number_of_vertices, int number_of_edges, int source){
+	int adj_matrix[number_of_vertices][number_of_vertices];
+	
+	for(int i=0; i<number_of_vertices; i++)
+		for(int j=0; j<number_of_vertices; j++)
+			adj_matrix[i][j]= (i===j) ?0 :infinity;
+		
+	for(int i=0; i<number_of_edges; i++){
+		adj_matrix[edges[i].src][edges[i].dest]= edges[i].weight;
 	}
+	
+	int visited[number_of_vertices];
+	for(int i=0; i<number_of_vertices; i++)
+		visited=(i==source) ?1 :0;
 
+	for(int i=0; i<number_of_vertices-1; i++){
+		int ind= find_min(adj_matrix[0], visited, number_of_vertices);
+		if(ind==-1)
+			break;
+		for(int i=0; i<no_of_vertices; i++){
+			int cost= adj_matrix[0][i]+adj_matrix[0][i];
+			if(adj_matrix[ind][i]<=cost)
+				adj_matrix[0][i]= cost;
+		}
+	}
+}
+
+int main(void){
+//Fetching the graph.
+	int e,v; cout << "\nNumber of vertices & edges> "; 
+	cin >> v >> e;
+
+	node *edges[e];
+	cout << "Drop weight, source, and destination of " << e << " edges.\n";
+	int w,s,d;
+	for(int i=0; i<e; i++){
+		cin >> w >> s >> d;
+		edges[i]= new node(w, s, d);
+	}
+	
+	//kruskal_MST(edges, v, e);
 
 	return 0;
 }
--- Graph-Theory/DisjointSet-CollapseFind.cpp
+++ Graph-Theory/DisjointSet-CollapseFind.cpp
@@ -53,33 +53,63 @@ int* find(int* rel, int src){
 	return data;
 }
 
-bool cycle_exists(int* relation, int v, int src, int dest){
-	return 1;
+
+
+bool cycle_exists(int* relation,int u, int v){
+	//[Data]::0th->Parent 1st->Rank
+	int *u_data= find(relation, u), *v_data= find(relation, v);
+	if(u_data[0]==v_data[0])
+		return 1;
+	else{
+		if(u_data[1]<v_data[1]){
+			relation[u_data[0]]+= relation[v_data[0]];
+			relation[v]= u_data[0];
+		}
+		else{
+			relation[v_data[0]]+= relation[u_data[0]];
+			relation[u]= v_data[0];
+		}
+		return 0;
+	}
+}
+
+void kruskal_MST(node** edges, int no_of_vertices, int no_of_edges){
+	quicksort_weights(edges, 0, no_of_edges-1);
+	int rel[no_of_vertices];for(int i=0; i<no_of_vertices; i++) rel[i]= -1;
+	
+	cout << "Edges in the Spanning-Tree, rendered via Kruskal Algo>\n\n";
+	int edge=0, u, v, min_cost=0, mst_edge=0;
+	while(mst_edge< no_of_vertices && edge<no_of_edges){
+		u= edges[edge]->src;
+		v= edges[edge]->dest;
+		if(!cycle_exists(rel, u, v)){
+			min_cost+= edges[edge]->weight;
+			cout << u << " -> " << v << endl;
+			mst_edge++;
+		}
+		edge++;
+	}
+
+	if(mst_edge==no_of_vertices-1)
+		cout << "\n\nYes this graph can be induced into MST with aforementioned edges.\nWeight(MST)> " << min_cost << endl;
+	else
+		cout << "\n\nNope! this graph can't be induced into MST :/" << endl;
 }
 
 int main(void){
 //Fetching the graph.
-	int e,v; cout << "Number of vertices & edges> "; 
+	int e,v; cout << "\nNumber of vertices & edges> "; 
 	cin >> v >> e;
 
-	node *vertex[e];
-	cout << "Drop Wt Src Dest of every edge>>\n";
+	node *edges[e];
+	cout << "Drop weight, source, and destination of " << e << " edges.\n";
 	int w,s,d;
 	for(int i=0; i<e; i++){
 		cin >> w >> s >> d;
-		vertex[i]= new node(w, s, d);
+		edges[i]= new node(w, s, d);
 	}
-	quicksort_weights(vertex, 0, e-1);
 	
-	int rel[v];
-	for(int i=0; i<v; i++)
-		rel[i]= -1;
-
-	//0th->Parent 1st->Rank
-	int *relation= find(rel, 3);
-	cout << relation[0] << " " << relation[1] << endl;
+	kruskal_MST(edges, v, e);
 
 	return 0;
 }
-
-
